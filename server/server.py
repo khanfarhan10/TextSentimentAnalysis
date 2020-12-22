cc6c9e76f876d10d8890dad1be6b10aac3f0bf55
Vamsi995 <saivamsi.ds123@gmail.com>
2020-12-22 18:29:06 +0530

    integrating the model into flask

--- server/server.py
+++ server/server.py
@@ -1,13 +1,26 @@
 from flask import Flask, request
+from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification
+import torch
 
 app = Flask(__name__)
 
+tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-cased");
+model = DistilBertForSequenceClassification.from_pretrained("/models/amazon-distilbert", local_files_only=False)
 
 @app.route("/run_forward", methods=["POST"])
 def forward():
     params = request.get_json()
     sentence = params["sentence"]
     print(sentence)
+
+    tokens = tokenizer(sentence, return_tensors="pt")
+
+    output = model(tokens["input_ids"], tokens["attention_mask"]).logits
+
+    probs = torch.softmax(output, dim=1).tolist()[0]
+
+    print(probs)
+
     return {"data": sentence}
 
 
